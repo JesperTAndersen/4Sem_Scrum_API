38e0abce53f55155080c0f5089ab66486c1b23c7
Jesper TA <Axicon89@gmail.com>
2026-09-10 16:18:41 +0200

    added CompetenceController and updated DependencyContainer to include CompetenceService

--- src/main/java/app/config/DependencyContainer.java
+++ src/main/java/app/config/DependencyContainer.java
@@ -1,13 +1,17 @@
 package app.config;
 
 import app.config.hibernate.HibernateConfig;
+import app.controllers.CompetenceController;
 import app.controllers.SecurityController;
 import app.controllers.routes.Routes;
+import app.dao.CompetenceDAO;
+import app.services.CompetenceServiceImpl;
 import jakarta.persistence.EntityManagerFactory;
 
 public class DependencyContainer
 {
     private final SecurityController securityController;
+    private final CompetenceController competenceController;
 
     public DependencyContainer()
     {
@@ -16,11 +20,15 @@ public class DependencyContainer
 
     public DependencyContainer(EntityManagerFactory emfTest)
     {
+        CompetenceDAO competenceDAO = new CompetenceDAO(emfTest);
+        CompetenceServiceImpl competenceService = new CompetenceServiceImpl(competenceDAO);
+
+        this.competenceController = new CompetenceController(competenceService);
         this.securityController = new SecurityController();
     }
 
     public Routes getRoutes()
     {
-        return new Routes(securityController);
+        return new Routes(securityController, competenceController);
     }
 }
