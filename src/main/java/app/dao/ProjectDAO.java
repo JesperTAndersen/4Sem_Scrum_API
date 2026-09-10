939def9f383d7c1e16749d936e8d62f5f0390852
Obliwobbi <dev@obli.dk>
2026-09-10 23:12:24 +0200

    added ProjectDAO implementation for CRUD operations

---
+++ src/main/java/app/dao/ProjectDAO.java
@@ -0,0 +1,26 @@
+package app.dao;
+
+import app.dao.interfaces.IProjectDAO;
+import app.entities.Project;
+
+public class ProjectDAO implements IProjectDAO
+{
+
+    @Override
+    public Project create(Project entity)
+    {
+        return null;
+    }
+
+    @Override
+    public Project read(Project entity)
+    {
+        return null;
+    }
+
+    @Override
+    public Project update(Project entity)
+    {
+        return null;
+    }
+}
