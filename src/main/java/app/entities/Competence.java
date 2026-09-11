3b27e683c3f549b1fc8bff61d9110e2c732016c1
Jesper TA <Axicon89@gmail.com>
2026-09-11 08:49:25 +0200

    implemented custom equals and hashCode methods in Competence entity for proper comparison

--- src/main/java/app/entities/Competence.java
+++ src/main/java/app/entities/Competence.java
@@ -1,15 +1,16 @@
 package app.entities;
 
-import jakarta.persistence.Entity;
 import jakarta.persistence.*;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.proxy.HibernateProxy;
 
 import java.math.BigDecimal;
+import java.util.Objects;
 
 @Getter
 @NoArgsConstructor
-@Builder
-@EqualsAndHashCode
 @AllArgsConstructor
 @Entity
 @Table(name = "competences")
@@ -25,4 +26,22 @@ public class Competence
 
     @Column(name = "rate", nullable = false)
     private BigDecimal rate;
+
+    @Override
+    public final boolean equals(Object o)
+    {
+        if (this == o) return true;
+        if (o == null) return false;
+        Class<?> oEffectiveClass = o instanceof HibernateProxy ? ((HibernateProxy) o).getHibernateLazyInitializer().getPersistentClass() : o.getClass();
+        Class<?> thisEffectiveClass = this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass() : this.getClass();
+        if (thisEffectiveClass != oEffectiveClass) return false;
+        Competence that = (Competence) o;
+        return getId() != null && Objects.equals(getId(), that.getId());
+    }
+
+    @Override
+    public final int hashCode()
+    {
+        return this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass().hashCode() : getClass().hashCode();
+    }
 }
