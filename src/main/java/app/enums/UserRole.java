65470f138118c3c3fde3cba18101dc8f0b0f4ffc
Morten Jensen <mo_ten@hotmail.com>
2026-09-11 08:47:21 +0200

    implemented user entity with validation, and password util for checking password

---
+++ src/main/java/app/enums/UserRole.java
@@ -0,0 +1,6 @@
+package app.enums;
+
+public enum UserRole
+{
+    PROJECT_MANAGER
+}
---
+++ src/main/java/app/utils/PasswordUtil.java
@@ -0,0 +1,26 @@
+package app.utils;
+
+import org.mindrot.jbcrypt.BCrypt;
+
+public class PasswordUtil
+{
+    private PasswordUtil(){}
+
+    public static String hashPassword(String plainPassword, int salt)
+    {
+        if (plainPassword == null || plainPassword.isEmpty())
+        {
+            throw new IllegalArgumentException("Password cannot be empty or null");
+        }
+        return BCrypt.hashpw(plainPassword, BCrypt.gensalt(salt));
+    }
+
+    public static boolean verifyPassword(String plainPassword, String hashedPassword)
+    {
+        if (plainPassword == null || plainPassword.isBlank())
+        {
+            return false;
+        }
+        return BCrypt.checkpw(plainPassword, hashedPassword);
+    }
+}
