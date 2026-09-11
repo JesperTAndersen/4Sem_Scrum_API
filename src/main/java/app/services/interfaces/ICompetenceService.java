1ee4b5382db24c10db576cb6a97e0c7c61375fba
Jesper TA <Axicon89@gmail.com>
2026-09-11 13:36:48 +0200

    refactor: streamline competence service by extending ICrudService and remove StageService

--- src/main/java/app/services/interfaces/ICompetenceService.java
+++ src/main/java/app/services/interfaces/ICompetenceService.java
@@ -2,17 +2,6 @@ package app.services.interfaces;
 
 import app.dtos.CompetenceDTO;
 
-import java.util.List;
-
-public interface ICompetenceService
+public interface ICompetenceService extends ICrudService<CompetenceDTO, CompetenceDTO>
 {
-    CompetenceDTO create(CompetenceDTO dto);
-
-    CompetenceDTO get(Long id);
-
-    List<CompetenceDTO> getAll();
-
-    CompetenceDTO update(CompetenceDTO dto);
-
-    void delete(Long id);
 }
---
+++ src/main/java/app/services/interfaces/IStageService.java
@@ -0,0 +1,8 @@
+package app.services.interfaces;
+
+import app.dtos.stage.StageCreateDTO;
+import app.dtos.stage.StageDTO;
+
+public interface IStageService extends ICrudService<StageCreateDTO, StageDTO>
+{
+}
