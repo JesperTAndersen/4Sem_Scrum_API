a19a7c9206238170e67f0d0d12964b59ab73b1d7
DHangaard <124468537+DHangaard@users.noreply.github.com>
2026-09-21 01:45:20 +0200

    feature(exception): update DatabaseException to project

--- src/main/java/app/exceptions/DatabaseException.java
+++ src/main/java/app/exceptions/DatabaseException.java
@@ -1,15 +1,16 @@
 package app.exceptions;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import io.javalin.http.HttpStatus;
 
-public class DatabaseException extends RuntimeException
+public class DatabaseException extends ApiException
 {
-    private static final Logger LOGGER = LoggerFactory.getLogger(DatabaseException.class);
+    public DatabaseException(String message)
+    {
+        super(HttpStatus.INTERNAL_SERVER_ERROR.getCode(), message);
+    }
 
     public DatabaseException(String message, Throwable cause)
     {
-        super(message, cause);
-        LOGGER.error("Database error: {} | Cause: {}", message, cause.getMessage(), cause);
+        super(HttpStatus.INTERNAL_SERVER_ERROR.getCode(), message, cause);
     }
 }
