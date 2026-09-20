a2797d1d1392d7a52ae384311fdd3fd003606ae0
DHangaard <124468537+DHangaard@users.noreply.github.com>
2026-09-21 01:46:03 +0200

    feature(exception): update BadRequestException

--- src/main/java/app/exceptions/BadRequestException.java
+++ src/main/java/app/exceptions/BadRequestException.java
@@ -2,9 +2,9 @@ package app.exceptions;
 
 import io.javalin.http.HttpStatus;
 
-public class ValidationException extends ApiException
+public class BadRequestException extends ApiException
 {
-    public ValidationException(String message)
+    public BadRequestException(String message)
     {
         super(HttpStatus.BAD_REQUEST.getCode(), message);
     }
